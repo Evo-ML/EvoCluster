e86bdd9c3005ee921eb14a1761dfd05130f44f67
Raneem Qaddoura <raneem.qaddoura@gmail.com>
2021-05-31 15:51:34 +0300

    update setup file

--- setup.py
+++ setup.py
@@ -1,20 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 20 17:06:19 2021
+
+@author: Dang Trung Anh
+"""
+
+import os
+from pip._internal.network.session import PipSession
 from setuptools import setup, find_packages
 
+VERSION = '1.0.1' 
+DESCRIPTION = 'An Open-Source Nature-Inspired Optimization Clustering Framework in Python'
+LONG_DESCRIPTION = 'An Open-Source Nature-Inspired Optimization Clustering Framework in Python'
+
+from pip._internal.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+reqs = [str(ir.requirement) for ir in install_reqs]
+
 setup(
-    name="EvoCluster",
-    version="1.0.0",
-    author="Raneem Qaddoura, Hossam Faris, and Ibrahim Aljarah",
-    author_email="raneem.qaddoura@gmail.com",
-    description="An Open-Source Nature-Inspired Optimization Clustering Framework in Python",
-    long_description="",
-    long_description_content_type="text/markdown",
-    url="https://github.com/RaneemQaddoura/EvoCluster",
-    packages=find_packages(where='src'),
-    package_dir={"": "src"},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache License 2.0",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
+       # the name must match the folder name 'EvoCC'
+        name="EvoCluster", 
+        version=VERSION,
+        author="Raneem Qaddoura, Hossam Faris, and Ibrahim Aljarah",
+        author_email="dangtrunganh@gmail.com, raneem.qaddoura@gmail.com",
+        description="An Open-Source Nature-Inspired Optimization Clustering Framework in Python",
+        long_description=LONG_DESCRIPTION,
+        # package_dir={"": "EvoCluster"},
+        packages=find_packages(),
+        # add any additional packages that 
+        install_requires=reqs,
+        url="https://github.com/RaneemQaddoura/EvoCluster",
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ],
+        package_data = {'': ['datasets/*.csv']},
+        
 )
