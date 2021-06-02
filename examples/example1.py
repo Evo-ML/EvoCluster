84fc36abdc4223734cb5a575c7c3ffb728ff3b29
Raneem Qaddoura <raneem.qaddoura@gmail.com>
2021-06-02 18:02:36 +0300

    Update example1.py

--- examples/example1.py
+++ examples/example1.py
@@ -7,13 +7,13 @@ Created on Thu May 20 17:06:19 2021
 
 from EvoCluster import EvoCluster
 
-optimizer = ["SSA", "PSO", "GA", "GWO"]
-objective_func = ["SSE", "TWCV"]
-dataset_list = ["iris", "aggregation"]
-num_of_runs = 3
-params = {'PopulationSize': 30, 'Iterations': 50}
+optimizer = ["SSA", "PSO", "GA", "GWO"] #Select optimizers from the list of available ones: "SSA","PSO","GA","BAT","FFA","GWO","WOA","MVO","MFO","CS".
+objective_func = ["SSE", "TWCV"] #Select objective function from the list of available ones:"SSE","TWCV","SC","DB","DI".
+dataset_list = ["iris", "aggregation"] #Select data sets from the list of available ones
+num_of_runs = 3 #Select number of repetitions for each experiment. 
+params = {'PopulationSize': 30, 'Iterations': 50} #Select general parameters for all optimizers (population size, number of iterations)
 export_flags = {'Export_avg': True, 'Export_details': True, 'Export_details_labels': True,
-                'Export_convergence': True, 'Export_boxplot': True}
+                'Export_convergence': True, 'Export_boxplot': True} #Choose some parameters
 
 ec = EvoCluster(
     optimizer,
